ff5188faa1c326aa9ce815ad007865b7074eabc7
Alyssa Chollet <apchollet@willamette.edu>
2024-10-28 19:38:27 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,45 +1,37 @@
 ##################################################
-# Name:
+# Name: Aly Chollet
 # Collaborators:
-# Estimate of time spent (hr):
+# Estimate of time spent (hr):2 
 ##################################################
 
 def is_magic_square(array:list[list[int]]) -> bool:
     row1 = 0
     row2 = 0
     row3 = 0
-    '''if small[0][0] + small[0][1] + small[0][2] == 15:
-        print(True)
-    else:
-        print(False)'''
+    is_magic = 0
 
     for row in small:
         if sum(row) == 15:
-            print('sum row=',sum(row),',True')
+            is_magic += 1
         row1 += row[0]
         row2 += row[1]
         row3 += row[2]
         if row1 == 15:
-            print("r1= true")
+            is_magic += 1
         if row2 == 15:
-            print("r2= true")
+            is_magic += 1
         if row3 == 15:
-            print("r3= true")
+            is_magic += 1
+    if sum(small[i][i] for i in range(3)):
+        is_magic += 1
+    if sum(small[i][2-i] for i in range(3)):
+        is_magic += 1
+    if is_magic == 8:
+        return True
+    else:
+        return False
 
-
-
-
-
-
-
-    #check if the list is greater than 3, return false
-
-
-
-
-
-
-small = [[8,1,6],[3,5,7],[4,9,2]]
+small = [[8,1,6,],[3,5,7],[4,9,2]]
 print(is_magic_square(small))
 
 
